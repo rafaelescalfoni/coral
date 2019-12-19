c577de500b5ac0bbf68b3d964e251fb2fa6efe44
Leonardo Marinho <leonardomarinho.nf@gmail.com>
2019-12-18 21:27:24 -0300

    Autenticação

--- core/views.py
+++ core/views.py
@@ -1,23 +1,36 @@
-from django.shortcuts import render
+from django.contrib.auth import login as auth_login, authenticate, logout as auth_logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import *
 
 def login(request):
-    # general_error = ""
+    general_error = ""
 
-    # if request.POST:
-    #     form = AuthenticationForm(request=request, data=request.POST)
-    #     if form.is_valid():
-    #         username = form.cleaned_data.get("username")
-    #         password = form.cleaned_data.get("password")
-    #         user = authenticate(username=username, password=password)
+    if request.POST:
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
 
-    #         if user is not None:
-    #             auth_login(request, user)
-    #             general_error = f"Você já está logado(a) como {username}"
-    #             return redirect("/story")
-    #         else:
-    #             general_error = "Usuário ou senha inválidos"
-    #     else:
-    #         general_error = "Usuário ou senha inválidos"
+            if user is not None:
+                auth_login(request, user)
+                general_error = f"Você já está logado(a) como {username}"
+                return redirect("/home")
+            else:
+                general_error = "Usuário ou senha inválidos"
+        else:
+            general_error = "Usuário ou senha inválidos"
 
-    # form = AuthenticationForm()
-    return render(request, "auth/login.html")#, {"form": form, "general_error": general_error})
+    form = AuthenticationForm()
+    return render(request, "auth/login.html", {"form": form, "general_error": general_error})
+
+def logout(request):
+    auth_logout(request)
+    return redirect("/login")
+
+@login_required
+def home(request):
+    return render(request, "home.html", {})
